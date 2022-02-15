c204d9f11e7da59154e17011581aeb20e0cbcc35
Deivid Medina <deivid.medina@mail.escuelaing.edu.co>
2022-02-15 15:34:16 -0500

    Terminando ejercicio 2 Arep.

--- src/main/java/edu/escuelaing/arep/app/Ejercicios/Ejercicio2_Arep.java
+++ src/main/java/edu/escuelaing/arep/app/Ejercicios/Ejercicio2_Arep.java
@@ -1,9 +1,6 @@
 package edu.escuelaing.arep.app.Ejercicios;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
@@ -13,34 +10,34 @@ import java.util.regex.Pattern;
 
 public class Ejercicio2_Arep {
     public static void main(String[] args) throws Exception {
+       extraerDatosURL(leerURL());
+    }
+
+    private static URL leerURL() throws MalformedURLException {
         Scanner input = new Scanner(System.in);
         String urlIngresada = input.nextLine();
         URL newURL = new URL(urlIngresada);
-        String contenidoURL = extraerDatosURL(newURL);
-        crearArchivoHtml();
+        return newURL;
     }
 
-    private static String extraerDatosURL(URL urlExtraer) throws Exception {
-        String contenido = "";
+    private static void extraerDatosURL(URL urlExtraer) throws Exception {
+        File archvivo = new File("src/main/java/edu/escuelaing/arep/app/Ejercicios/resultado.html");
         try {
             URLConnection urlConnection = urlExtraer.openConnection(); //objeto que utilizamos para examinar las propiedades del recurso remoto referenciado o para obtener su contenido
             InputStream is = urlConnection.getInputStream(); //declara los métodos para leer datos desde una fuente concreta
             BufferedReader br = new BufferedReader(new InputStreamReader(is)); // para leer el texto de una secuencia de entrada (como un archivo) almacenando en el búfer
+            BufferedWriter bw = new BufferedWriter(new FileWriter(archvivo));
             String linea = br.readLine();
             while (linea != null){
-                contenido = linea;
+                bw.write(linea);
                 linea = br.readLine();
             }
+            br.close();
+            bw.close();
         }catch (MalformedURLException e){
             e.printStackTrace();
         }catch (IOException e) {
             e.printStackTrace();
         }
-        return contenido;
     }
-
-    private static void crearArchivoHtml(){
-
-    }
-
 }
